e2c8d6cad4587fedd5b2e6ec58e39de525d75807
sepehr <sepehrgh01@gmail.com>
2020-08-09 14:18:11 +0430

    Fixed issue with out of range numeric value on VerificationResponse

--- src/main/java/com/youtopin/zarinpal/domain/VerificationResponse.java
+++ src/main/java/com/youtopin/zarinpal/domain/VerificationResponse.java
@@ -8,7 +8,7 @@ import lombok.Setter;
 @Setter
 public class VerificationResponse {
     @JsonProperty("RefID")
-    private Integer refId;
+    private Long refId;
     @JsonProperty("Status")
     private Integer status;
 }
